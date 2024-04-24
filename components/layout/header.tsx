a6668b35ac7f9c70f619375f1283af9ad819a4cb
Vinh Pham <vinhpham4895@gmail.com>
2024-04-24 20:39:37 +0700

    Update UI

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Box, Button, Select, Drawer } from "@mantine/core";
+import { Box, Group, Button, Select, Drawer, CloseButton } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconMenu2, IconSearch } from "@tabler/icons-react";
+import { IconMenu2, IconSearch, IconX } from "@tabler/icons-react";
 
 export default function Header() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -19,29 +19,37 @@ export default function Header() {
         }
         position="top"
         size="100%"
+        closeButtonProps={{
+          icon: <CloseButton size="50px" bg="yellow.5" radius="xl" p="xs" />,
+        }}
+        transitionProps={{ duration: 300 }}
       >
-        Drawer content
+        Drawer Content
       </Drawer>
-      <Box h="48px" bg="#121212">
-        <Button color="yellow.5" c="black">
-          IMDb
-        </Button>
-        <Button
-          leftSection={<IconMenu2 />}
-          variant="subtle"
-          color="white"
-          c="white"
-          onClick={open}
-        >
-          Menu
-        </Button>
-        <Select
-          placeholder="Search IMDb"
-          rightSection={<IconSearch />}
-          w="500px"
-          searchable
-          data={["Movie 1", "Movie 2", "Movie 3"]}
-        />
+      <Box h="48px" px="12px" bg="#121212">
+        <Group h="32px">
+          <Button color="yellow.5" c="black" h="100%">
+            IMDb
+          </Button>
+          <Button
+            leftSection={<IconMenu2 />}
+            variant="subtle"
+            color="white"
+            c="white"
+            onClick={open}
+            h="100%"
+          >
+            Menu
+          </Button>
+          <Select
+            placeholder="Search IMDb"
+            rightSection={<IconSearch />}
+            w="500px"
+            searchable
+            data={["Movie 1", "Movie 2", "Movie 3"]}
+            styles={{ input: { height: "100%" } }}
+          />
+        </Group>
       </Box>
     </>
   );
